59f25a9cad403389fb0c4efb0161cf676eb69e2a
Yannys-coder <ccyannys123@gmail.com>
2025-04-22 08:41:15 -0400

    SUBMIT

--- main.py
+++ main.py
@@ -27,3 +27,31 @@ def details():
         (username, phone_number, number_of_items, total_amount, current_date)
         )
     mydb.commit()
+    return render_template('page.html')
+@app.route("/winner", methods=['POST', 'GET'])
+def winner():
+    mydb = mysql.connector.connect(
+         host="remotemysql.com",
+        user="uL7j9FaRVB",
+        password="qQvo5rGf3i",
+        database="uL7j9FaRVB")
+    mycursor = mydb.cursor()
+    mycursor.execute(
+        'SELECT * FROM Customer_Details WHERE date_of_purchase = current_date order by Total_Amount DESC'
+    )
+    account = mycursor.fetchone()
+    print(account)
+    if account:
+        user_name = account[0]
+        phone_number = account[1]
+        return render_template('page.html', user_name = user_name, phone_number = phone_number)
+    else:
+        return render_template('page.html')
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+app.run()
+
+
+    
